4074659a5d4c7738c33ab1c67e261988638906f8
Muhtadi1Laskar <laskar90muhtadi@gmail.com>
2025-10-20 03:25:07 +0600

    Fixed code to run locally

--- api/index.js
+++ api/index.js
@@ -1,6 +1,3 @@
 import app from "../src/app.js";
-// import serverless from "serverless-http";
-
-// export const handler = serverless(app);
 
 export default app;
--- src/server.js
+++ src/server.js
@@ -1,10 +1,10 @@
-// import dotenv from "dotenv";
-// import app from "./app-dev.js";
+import dotenv from "dotenv";
+import app from "./app.js";
 
-// dotenv.config();
+dotenv.config();
 
-// const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 8080;
 
-// app.listen(PORT, () => {
-//   console.log(`Server is running on port ${PORT}`);
-// });
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
