b554e3a930aaddc994e4f653a6cca7abeaea71ae
Muhtadi1Laskar <laskar90muhtadi@gmail.com>
2025-11-20 18:43:34 +0600

    Added json limit

--- src/app.js
+++ src/app.js
@@ -9,7 +9,7 @@ const app = express();
 
 app.use(cors());
 app.use(helmet());
-app.use(express.json());
+app.use(express.json({ limit: "5mb" }));
 app.use(morgan("dev"));
 
 app.use("/api", router);
